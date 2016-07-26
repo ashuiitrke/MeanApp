d2e4e4234cc9fc6c46fe88b06cf560c82e98bcda
ashuiitkre <ashuiitrke@gmail.com>
2016-07-27 01:09:11 +0530

    port 80 2

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var passport = passport();
 app.listen(80);
 
 // Log the server status to the console
-console.log('Server running at http://localhost:3000/');
+console.log('Server running at http://localhost:80/');
 
 // Use the module.exports property to expose our Express application instance for external usage
 module.exports = app;
