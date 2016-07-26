c1f9aeb02248a0358e8ad07bf747a005072e3d3d
ashuiitkre <ashuiitrke@gmail.com>
2016-07-27 01:06:20 +0530

    port 80

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var app = express(db);
 var passport = passport();
 
 // Use the Express application instance to listen to the '3000' port
-app.listen(3000);
+app.listen(80);
 
 // Log the server status to the console
 console.log('Server running at http://localhost:3000/');
